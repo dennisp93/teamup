321e0aa01358a46bb53cd8e9f6f4d40ecaba3fa9
Dennis Paulig <dennis.paulig@i22.de>
2017-12-15 18:12:33 +1300

    adds router component with initial routing for root

--- frontend/src/Router/Router.js
+++ frontend/src/Router/Router.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import App from "../App";
+
+class Router extends Component {
+  render() {
+    return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </BrowserRouter>
+    )
+  }
+}
+
+export default Router
