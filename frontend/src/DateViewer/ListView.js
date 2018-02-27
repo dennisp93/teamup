f62df0154487046b8201a722afdc0338c2501e8f
Dennis Paulig <dennis.paulig@i22.de>
2018-02-28 09:45:01 +1300

    list view optimization

--- frontend/src/DateViewer/ListView.js
+++ frontend/src/DateViewer/ListView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ListView.css';
 
 class ListView extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class ListView extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>Kommende Termine</h2>
         {this.state.dates.map((date, key) => this.renderDate(date, key))}
       </div>
@@ -29,14 +30,14 @@ class ListView extends Component {
 
   renderDate(date, key) {
     return (
-      <ul key={key}>
-        <li>{date.type}</li>
-        <li>{date.day}</li>
-        <li>{date.time}</li>
-        <li>{date.state}</li>
-        <li><button onClick={() => this.changeState(key, 'available')}>Zusagen</button></li>
-        <li><button onClick={() => this.changeState(key, 'open')}>Offen</button></li>
-        <li><button onClick={() => this.changeState(key, 'unavailable')}>Absagen</button></li>
+      <ul key={key} className="date">
+        <li className="type">{date.type}</li>
+        <li className="day">{date.day}</li>
+        <li className="time">{date.time} Uhr</li>
+        <li className={"state " + date.state}>{date.state}</li>
+        <li><button onClick={() => this.changeState(key, 'available')} className="available">Zusagen</button></li>
+        <li><button onClick={() => this.changeState(key, 'open')} className="open">Offen</button></li>
+        <li><button onClick={() => this.changeState(key, 'unavailable')} className="unavailable">Absagen</button></li>
       </ul>
     );
   }
